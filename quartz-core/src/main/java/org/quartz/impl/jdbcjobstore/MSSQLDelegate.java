ccf5ed356ed51fd4b0af330eca80b2b1a1a3fba1
arnaud.mergey@semarchy.com <arnaud.mergey@semarchy.com>
2024-07-29 13:57:25 +0200

    fix MSSQL server issue + fix osgi metadata issue + fix versionning

--- quartz-core/src/main/java/org/quartz/impl/jdbcjobstore/MSSQLDelegate.java
+++ quartz-core/src/main/java/org/quartz/impl/jdbcjobstore/MSSQLDelegate.java
@@ -17,14 +17,20 @@
 
 package org.quartz.impl.jdbcjobstore;
 
+import static org.quartz.TriggerKey.triggerKey;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
-import org.quartz.spi.ClassLoadHelper;
-import org.slf4j.Logger;
+import org.quartz.TriggerKey;
 
 /**
  * <p>
@@ -35,33 +41,30 @@ import org.slf4j.Logger;
  */
 public class MSSQLDelegate extends StdJDBCDelegate {
 
-    //---------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------
     // protected methods that can be overridden by subclasses
-    //---------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------
 
     /**
      * <p>
-     * This method should be overridden by any delegate subclasses that need
-     * special handling for BLOBs. The default implementation uses standard
-     * JDBC <code>java.sql.Blob</code> operations.
+     * This method should be overridden by any delegate subclasses that need special
+     * handling for BLOBs. The default implementation uses standard JDBC
+     * <code>java.sql.Blob</code> operations.
      * </p>
      * 
-     * @param rs
-     *          the result set, already queued to the correct row
-     * @param colName
-     *          the column name for the BLOB
+     * @param rs      the result set, already queued to the correct row
+     * @param colName the column name for the BLOB
      * @return the deserialized Object from the ResultSet BLOB
-     * @throws ClassNotFoundException
-     *           if a class found during deserialization cannot be found
-     * @throws IOException
-     *           if deserialization causes an error
+     * @throws ClassNotFoundException if a class found during deserialization cannot
+     *                                be found
+     * @throws IOException            if deserialization causes an error
      */
-    @Override           
+    @Override
     protected Object getObjectFromBlob(ResultSet rs, String colName)
-        throws ClassNotFoundException, IOException, SQLException {
+            throws ClassNotFoundException, IOException, SQLException {
         InputStream binaryInput = rs.getBinaryStream(colName);
 
-        if(binaryInput == null || binaryInput.available() == 0) {
+        if (binaryInput == null || binaryInput.available() == 0) {
             return null;
         }
 
@@ -77,14 +80,52 @@ public class MSSQLDelegate extends StdJDBCDelegate {
         return obj;
     }
 
-    @Override           
+    @Override
     protected Object getJobDataFromBlob(ResultSet rs, String colName)
-        throws ClassNotFoundException, IOException, SQLException {
+            throws ClassNotFoundException, IOException, SQLException {
         if (canUseProperties()) {
             InputStream binaryInput = rs.getBinaryStream(colName);
             return binaryInput;
         }
         return getObjectFromBlob(rs, colName);
+    }
+
+    @Override
+    public List<TriggerKey> selectTriggerToAcquire(Connection conn, long noLaterThan, long noEarlierThan, int maxCount)
+            throws SQLException {
+        if (maxCount < 1)
+            maxCount = 1; // we want at least one trigger back.
+
+        String selectTriggerToAcquire = "SELECT TOP " + maxCount + " " + SELECT_NEXT_TRIGGER_TO_ACQUIRE.substring(6);
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<TriggerKey> nextTriggers = new LinkedList<TriggerKey>();
+        try {
+            ps = conn.prepareStatement(rtp(selectTriggerToAcquire));
+
+            // Set max rows to retrieve
+            ps.setMaxRows(maxCount);
+
+            // Try to give jdbc driver a hint to hopefully not pull over more than the few
+            // rows we actually need.
+            // Note: in some jdbc drivers, such as MySQL, you must set maxRows before
+            // fetchSize, or you get exception!
+            ps.setFetchSize(maxCount);
+
+            ps.setString(1, STATE_WAITING);
+            ps.setBigDecimal(2, new BigDecimal(String.valueOf(noLaterThan)));
+            ps.setBigDecimal(3, new BigDecimal(String.valueOf(noEarlierThan)));
+            rs = ps.executeQuery();
+
+            while (rs.next() && nextTriggers.size() < maxCount) {
+                nextTriggers.add(triggerKey(rs.getString(COL_TRIGGER_NAME), rs.getString(COL_TRIGGER_GROUP)));
+            }
+
+            return nextTriggers;
+        } finally {
+            closeResultSet(rs);
+            closeStatement(ps);
+        }
     }
 }
 
